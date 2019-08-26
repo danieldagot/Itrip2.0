f198f55f4e500f8d0dfb2c841040d607dbbec1c5
unknown <danieldagot@gmail.com>
2019-08-26 13:34:07 +0300

    wiki

--- back-end/routes/wikiApi.js
+++ back-end/routes/wikiApi.js
@@ -1,9 +1,21 @@
-const axios= require ('axios')
+const axios = require('axios')
 
-async function  wikiInfo( citystr) {  
-  let data= await axios.get(`http://en.wikipedia.org/w/api.php?action=opensearch&search='${citystr}'&format=json`)
-  return data.data
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+async function wikiInfo(citystr) {
+  let data = await axios.get(`http://en.wikipedia.org/w/api.php?action=opensearch&search='${citystr}'&format=json`)
+
+let urls = (data.data)
+console.log(urls[2].join(","));
+let dis = {"short" : urls[2][0] , "long" :  urls[2].join(",")  }
+  return dis
 
 }
 
 module.exports = wikiInfo
+
+
+wikiInfo("istael")
+
+
