7d24053044360277c28352efc2ccdc8419936ba6
unknown <danieldagot@gmail.com>
2019-08-28 12:17:10 +0300

    ddfs

--- back-end/server.js
+++ back-end/server.js
@@ -9,13 +9,13 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const router = require('./routes/api')
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
 
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    next()  
-})
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+//     next()  
+// })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -24,13 +24,13 @@ app.use(bodyParser.urlencoded({
 // app.use(express.static(path.join(__dirname, 'build')));
 
 // app.use(express.static(path.join(__dirname, 'public/main-layout')))
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')));
 
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/Itrip', {
     useNewUrlParser: true
 }).then(() => {
     mongoose.set('useFindAndModify', false);
-app.use(express.static(path.join(__dirname, 'build')));
     app.use('/', router)
 
     app.get('*', function (req, res) {
