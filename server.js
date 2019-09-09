d0ebe87f773c20dadf43bacb958ddd2416c4e79e
danieldagot <danieldagot@gmail.com>
2019-09-09 21:37:06 +0300

    ser

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const express = require('express')
 const app = express()
 //const port = process.env.PORT
 const path = require('path')
-let port = process.env.PORT||8089
+let port = process.env.PORT||8080
 
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-const router = require('./routes/api')
+const router = require('./back-end/routes/api')
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     next()  
 })
@@ -28,6 +27,9 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(path.join(__dirname, 'public/main-layout')))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'back-end')))
+app.use(express.static(path.join(__dirname, 'back-end/mongoose/models')))
+app.use(express.static(path.join(__dirname, 'back-end/routes')))
 
 mongoose.connect('mongodb://localhost/Itrip', {
     useNewUrlParser: true
@@ -43,7 +45,7 @@ mongoose.connect('mongodb://localhost/Itrip', {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
     
-    mongoose.connect(process.env.MONGO_URI||"mongodb://localhost/crm", { useNewUrlParser: true }).then(() => {
+    mongoose.connect(process.env.MONGO_URI||"mongodb://localhost/Itrip", { useNewUrlParser: true }).then(() => {
         app.listen(port, function (err, res) {
             console.log("the server runs on port " + port)
         })
