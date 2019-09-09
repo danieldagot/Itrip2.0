151579c81f89737712193ce737ad3ed6f8c30403
danieldagot <danieldagot@gmail.com>
2019-09-10 01:16:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 //const port = process.env.PORT
 const path = require('path')
-let port = process.env.PORT||8080
+let port = process.env.PORT || 8080
 
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
@@ -15,37 +15,39 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    next()  
+    next()
 })
 
-app.use(express.static(path.join(__dirname, 'build')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/Itrip", { useNewUrlParser: true }).then(() => {
+    app.use(express.static(path.join(__dirname, 'build')))
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }))
 
 
-app.use(express.static(path.join(__dirname, 'public/main-layout')))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'back-end')))
-app.use(express.static(path.join(__dirname, 'back-end/mongoose/models')))
-app.use(express.static(path.join(__dirname, 'back-end/routes')))
+    app.use(express.static(path.join(__dirname, 'public/main-layout')))
+    app.use(express.static(path.join(__dirname, 'public')))
+    app.use(express.static(path.join(__dirname, 'back-end')))
+    app.use(express.static(path.join(__dirname, 'back-end/mongoose/models')))
+    app.use(express.static(path.join(__dirname, 'back-end/routes')))
 
-mongoose.connect('mongodb://localhost/Itrip', {
-    useNewUrlParser: true
-}).then(() => {
-    mongoose.set('useFindAndModify', false);
+    mongoose.connect('mongodb://localhost/Itrip', {
+        useNewUrlParser: true
+    }).then(() => {
+        mongoose.set('useFindAndModify', false);
 
-    app.use('/', router)
-
-    
+        app.use('/', router)
 
 
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-    
-    mongoose.connect(process.env.MONGO_URI||"mongodb://localhost/Itrip", { useNewUrlParser: true }).then(() => {
+
+
+        app.get('*', function (req, res) {
+            res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        });
+
+
         app.listen(port, function (err, res) {
             console.log("the server runs on port " + port)
         })
